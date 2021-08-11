f1df7d46fdeebe82ec56b5f1b294d33a5dd1bca6
Alex Fischer <alex@quadrant.net>
2021-08-11 10:53:30 -0600

    Animator - use animation instead of transition to remove race conditions

--- extras/SimpleModalAnimator.js
+++ extras/SimpleModalAnimator.js
@@ -7,39 +7,36 @@
     ONLY animates an explicit call to SimpleModal.close()
     - no exit animation if the parent window closes the SimpleModal
 
-    On first appearance, html element transitions from:
-        class="SimpleModal-animating SimpleModal-opening SimpleModal-out" 
-    to:
-        class="SimpleModal-animating SimpleModal-opening" 
+    On first appearance, html element has:
+        class="SimpleModal-animating SimpleModal-opening"
 
-    After the entrance transition, "SimpleModal-animating" and "SimpleModal-opening" classes are removed.
+    After the entrance animation, classes are removed.
     
-    On close, html element transitions from:
+    On close, html element has:
         class="SimpleModal-animating SimpleModal-closing" 
-    to:
-        class="SimpleModal-animating SimpleModal-closing SimpleModal-out" 
 
-    You must ONLY define your transitions when .SimpleModal-animating
-    or .SimpleModal-opening/.SimpleModal-closing are present.
+    Use these classes to define appropriate animation properties.
 
-    Also, you MUST set a transition-duration on <html> when these classes are present, if you don't animate any properties on that element. Our code looks at the transition duration of that element to determine how long the transition takes.
+    Important:
+    You MUST set an animation-duration on the <html> element. That's the element we check to know when the animation completes.
+
+    Tip:
+    .SimpleModal-animating {
+        // define most animation properties here
+    }
+    .SimpleModal-closing {
+        animation-direction: reverse;
+        // make sure the offscreen state persists after animation completes, while window is closed
+        animation-fill-mode: forwards;
+    }
 */
 (function() {
 'use strict';
 
-function after_html_transition(callback) {
-    /*
-        Note: transitionend events are rather unreliable, so we read computed
-        transition-duration style value from <html>.
-
-        If you're animating some other element (ie. <body>), 
-        be sure to set a matching transition duration on <html>
-
-        Ie: html {transition: none 0.3s}
-    */
+function after_html_animation(callback) {
     var h = document.documentElement;
-    var transition_seconds = parseFloat(getComputedStyle(h).transitionDuration);
-    setTimeout(callback, transition_seconds*1000);
+    var animation_seconds = parseFloat(getComputedStyle(h).animationDuration);
+    setTimeout(callback, animation_seconds*1000);
 }
 
 // This is definitely not a modal window - nothing to do
@@ -58,25 +55,14 @@ if (window.name != ALREADY_LOADED) {
 
     var h = document.documentElement;
 
-    // Up to you to define "off screen" styles when <html> has this class
-    h.classList.add('SimpleModal-out');
-
     // Wait for load event before revealing - make sure stylesheets have loaded and have been applied
     addEventListener('load', function() {
-        /*
-            Note: we cannot set these before load.
-
-            If we do, some browsers will actually animate the window out (animate the style change triggered by adding SimpleModal-out, even when setting these classes after that one). 
-        */
-        h.classList.add('SimpleModal-animating');
         h.classList.add('SimpleModal-opening');
-
-        after_html_transition(function() {
-            h.classList.remove('SimpleModal-animating');
+        h.classList.add('SimpleModal-animating');
+        after_html_animation(function() {
             h.classList.remove('SimpleModal-opening');
+            h.classList.remove('SimpleModal-animating');
         });
-
-        h.classList.remove('SimpleModal-out');
     });
 }
 
@@ -86,14 +72,11 @@ if (window.name != ALREADY_LOADED) {
 var _SimpleModal_close = SimpleModal.close.bind(SimpleModal);
 window.SimpleModal.close = function(value, then) {
     var h = document.documentElement;
+    h.classList.add('SimpleModal-closing');
     h.classList.add('SimpleModal-animating');
-    h.classList.add('SimpleModal-leaving');
-
-    after_html_transition(function() {
+    after_html_animation(function() {
         _SimpleModal_close(value, then);
     });
-
-    h.classList.add('SimpleModal-out');
 }
 
 })()
